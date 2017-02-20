af88983ae78e66ba4e82fc50fe7fb705e86b6e3d
Shashwat <91shashwat@gmail.com>
2017-02-20 12:49:09 +0100

    Renamed mainActivity+added permission

--- src/com/example/testvlc/MainCamerAPI.java
+++ src/com/example/testvlc/MainCamerAPI.java
@@ -4,8 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.hardware.camera2.*;
 
-public class MainActivity extends Activity {
+public class MainCamerAPI extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
