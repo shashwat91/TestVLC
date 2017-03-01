e327818d0ecc2468741f2c97c4d758cf1b612c25
shashwat91 <91shashwat@gmail.com>
2017-03-01 17:32:46 +0100

    Added detector method

--- src/com/example/testvlc/ProcessImage.java
+++ src/com/example/testvlc/ProcessImage.java
@@ -7,7 +7,10 @@ import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Vector;
 
 import android.media.Image;
 import android.util.Log;
@@ -43,16 +46,68 @@ public class ProcessImage
 		
 		/*Initialising all Mat*/
 		Mat inp_image = Highgui.imread(lastPath);
-		Mat grey_img = null;
+		Mat grey_img = new Mat();
 		
 		save(inp_image,"convImage_");
 		
 		Imgproc.cvtColor(inp_image,grey_img,Imgproc.COLOR_RGB2GRAY); //Changing image to grey scale
 		save(grey_img,"greyImage_");
 		
+		int[][] returnMatrix = new int[10][10];
+		detector(grey_img, returnMatrix);
+		save(grey_img,"detector_");
+		
 		return 42; //Change to actual ID after processing	
 	}
 	
+	private void detector(Mat input, int[][] returnMatrix) 
+	{
+		Mat blurred_image = new Mat();
+		Mat otsu = new Mat();
+		Mat hierarchy = new Mat();
+		
+		List<MatOfPoint> contours_point = new ArrayList<MatOfPoint>();
+		//List<MatOfPoint2f> contours_point2f;
+		Vector<Point> center = new Vector<Point>();
+		Vector<Float> radius = new Vector<Float>();
+		
+		Size window = new Size(100,100);
+		Imgproc.blur(input, blurred_image, window);
+		Imgproc.threshold(blurred_image, otsu, 0, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);
+		Imgproc.findContours(otsu.clone(), contours_point,hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_NONE);
+		
+		int count = contours_point.size();
+				
+		for( int i=0; i < count; i++)
+		{
+			Point c = new Point();
+			float[] r = new float[count];
+			MatOfPoint2f  contours_point2f = new MatOfPoint2f( contours_point.get(i).toArray() );
+			Imgproc.minEnclosingCircle(contours_point2f, c, r);
+
+			if (r[i] >= 50)
+			{
+				center.add(c);
+				radius.add(r[i]);
+			}
+		}
+		
+		int count2 = center.size();
+		Scalar red = new Scalar(255,255,255);
+
+		//returnMatrix.resize(count2);
+		
+
+		for( int i = 0; i < count2; i++)
+		{
+			Core.circle(input, center.get(i), (int)radius.get(i).floatValue(), red, 3);
+			returnMatrix[i][0] = (int) center.get(i).x;
+			returnMatrix[i][1] = (int) center.get(i).y;
+			returnMatrix[i][2] = (int)radius.get(i).floatValue();
+		}
+		Log.d(TAG, "Detected Circles"+count2);
+	}
+
 	private void save(Image imagetosave, String name) throws IOException 
     {
 		ByteBuffer buffer = imagetosave.getPlanes()[0].getBuffer();
