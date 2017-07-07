a2d7835157c0a228efc64a3f113ce9a6378f89af
Shashwat <91shashwat@gmail.com>
2017-07-07 16:15:09 +0200

    Arduino code with Ton<Toff

--- Code_unequal/VLC_unequal_8bit_PWM2/VLC_unequal_8bit_PWM2.ino
+++ Code_unequal/VLC_unequal_8bit_PWM2/VLC_unequal_8bit_PWM2.ino
@@ -1,3 +1,4 @@
+// On-Off frequency should be in ratio 3:2 (or 40%-60% duty cycle)
 const int freq_on = 3000;
 const int freq_off = 2000;
 int value_on = 16000000 / 256 /  freq_on / 2;
@@ -6,14 +7,14 @@ const int value[2] = {value_off,value_on};
 
 const int ledPin1 = 5;
 const int ledPin2 = 10;
-const int pwmWidth = 2;
+const int pwmWidth = 3;
 
-byte data1[10] = {0x1, 0x2, 0x3, 0x4, 0x5,
-                  0x6, 0x7, 0x8, 0x9, 0xA};
-unsigned dataNumber = 7;
+byte data1[10] = {0x31, 0x82, 0x13, 0xE4, 0x75,
+                  0x66, 0x27, 0xA8, 0x49, 0xDA};
+unsigned dataNumber = 0;
 
-int counter1 = 4;
-const int packet1Size = 10;
+int counter1 = 0;
+const int packet1Size = 31;
 bool led1Status = false;
 byte packet1[packet1Size];
 
@@ -23,8 +24,8 @@ void initializeTimer()
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1  = 0;
-  
-  OCR1A = value[led1Status];      // compare match register 16MHz/256/2Hz
+
+  OCR1A = value[led1Status];            // compare match register 16MHz/256/2Hz
   TCCR1B |= (1 << WGM12);   // CTC mode
   TCCR1B |= (1 << CS12);    // 256 prescaler
   TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
@@ -34,25 +35,23 @@ void initializeTimer()
 ISR(TIMER1_COMPA_vect)
 {
  if(counter1 < packet1Size)
-  { 
-    // -- Switch led on if its off and current bit is 1
+ { 
     if(packet1[counter1] == 1 && led1Status == false)
     {
       led1Status = true;
       OCR1A = value[led1Status];
       digitalWrite(ledPin1,led1Status);
     }
-    // -- Switch led off if its on and current bit is 0
-    else if(packet1[counter1] == 0 && led1Status == true)
+    
+    if(packet1[counter1] == 0 && led1Status == true)
     {
       led1Status = false;
       OCR1A = value[led1Status];
       digitalWrite(ledPin1,led1Status);
     }
-  }
-  counter1++; // Go to next bit
-  
-  //reset counter and make next packet
+ }
+
+  counter1++;
   if(counter1 == packet1Size)
   {
      counter1 = 0;
@@ -63,7 +62,6 @@ ISR(TIMER1_COMPA_vect)
   }
 }
 
-// Function for parity check
 byte parity(byte x)
 {
    byte y;
@@ -71,45 +69,56 @@ byte parity(byte x)
    y = y ^ (y >> 2);
    y = y ^ (y >> 4);
    y = y ^ (y >> 8);
-   //y = y ^ (y >>16);
+   y = y ^ (y >>16);
    return y & 1;
 }
 
 void makePacket1()
 {
   int i=0,j=0;
-  while(i<3)
+  while(i<8)
   {
-    if(j<3)   // --first 3 bits of start of packet
+    if(j<4)
     {
       packet1[j++] = 0;
       continue;
     }
-
-    if(j==3)    // 4th bit start
-    {
-      packet1[j++] = 1;
-      continue;
-    }
     
-    byte tmp = (data1[dataNumber]) & (0x04>>i);
-    if(tmp == 0)  // -- bits for symbol 0
+    byte tmp = (data1[dataNumber]) & (0x80>>i);
+    if(tmp == 0)
     {
       packet1[j++] = 1; //part 1 of data
       packet1[j++] = 0; //part 2 of data
+      packet1[j++] = 0; //part 3 of data
       i++;
     }
-    else    // -- bits for symbol 1
+    else
     {
-      packet1[j++] = 0; //part 1 of data
+      packet1[j++] = 1; //part 1 of data
       packet1[j++] = 1; //part 2 of data
+      packet1[j++] = 0; //part 3 of data
       i++;
+    }
+
+    if(i==8)
+    {
+      if(parity(data1[dataNumber]))
+      {
+        packet1[j++] = 1; //part 1 of parity
+        packet1[j++] = 1; //part 2 of parity
+        packet1[j++] = 0; //part 3 of parity
+      }
+      else
+      {
+        packet1[j++] = 1; //part 1 of parity
+        packet1[j++] = 0; //part 2 of parity
+        packet1[j++] = 0; //part 3 of parity
+      }
     }
   }
 //  printPacket();
 }
 
-// Function for printing packets through serial port
 void printPacket()
 {
   Serial.print(data1[dataNumber],HEX);
@@ -127,9 +136,9 @@ void setup()
 {
   makePacket1();
   pinMode(ledPin1, OUTPUT);
-  Serial.begin(115200);
+//  Serial.begin(115200);
   delay(5000);
-  printPacket();
+//  printPacket();
   initializeTimer();
 }
 
